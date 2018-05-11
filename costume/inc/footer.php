b77b63c9d5f97f19182e4dbd5b01c3cdb340014d
justineraydungan <35917904+justineraydungan@users.noreply.github.com>
2018-05-12 00:47:51 +0800

    Added Rental Service w/ database

---
+++ costume/inc/footer.php
@@ -0,0 +1,29 @@
+<br />
+	<footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Webtech 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+	</body>
+</html>
+
+<?php
+//If you have use Older PHP Version, Please Uncomment this function for removing error 
+
+/*function array_column($array, $column_name)
+{
+	$output = array();
+	foreach($array as $keys => $values)
+	{
+		$output[] = $values[$column_name];
+	}
+	return $output;
+}*/
+?>
---
+++ costume/inc/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Webslesson | PHP Mysql Shopping Cart</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<meta name="description" content="">
+    	<meta name="author" content="">
+
+	    <!-- Bootstrap core CSS -->
+	    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- Custom styles for this template -->
+	    <link href="css/heroic-features.css" rel="stylesheet">
+	</head>
+	<body data-spy="scroll" data-target="#myScrollspy" data-offset="20">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="myScrollspy">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Costumes and Attires</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+			<div class="md-form active-cyan-2 mb-3">
+				<input class="form-control" type="text" placeholder="Search" aria-label="Search">
+			</div>
+            <li class="nav-item">
+              <a class="nav-link" href="#costumes">Costumes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="costumes.php">Rentals</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#attires">Attires</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#others">Others</a>
+             <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.html">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+		<br />
+		<div class="container">
+			<br />
+			<br />
+			<h3 align="center"><a href="costumes.php" class="btn btn-primary" role="button">Costumes</a>
+			<a href="attires.php" class="btn btn-primary" role="button">Attires</a></h3>
