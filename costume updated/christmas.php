be469a8f14dee68dc16fa35691238c25c57684c0
justineraydungan <35917904+justineraydungan@users.noreply.github.com>
2018-05-15 06:10:39 +0800

    Update Customer Module

--- costume updated/christmas.php
+++ costume updated/christmas.php
@@ -1,19 +1,19 @@
 <?php 
 session_start();
-$connect = mysqli_connect("localhost", "root", "", "rent");
+$connect = mysqli_connect("localhost", "root", "", "weblove_attirerentals");
 
 if(isset($_POST["add_to_cart"]))
 {
 	if(isset($_SESSION["shopping_cart"]))
 	{
-		$item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
-		if(!in_array($_GET["id"], $item_array_id))
+		$item_array_product_code = array_column($_SESSION["shopping_cart"], "item_product_code");
+		if(!in_array($_GET["product_code"], $item_array_product_code))
 		{
 			$count = count($_SESSION["shopping_cart"]);
 			$item_array = array(
-				'item_id'			=>	$_GET["id"],
+				'item_product_code'			=>	$_GET["product_code"],
 				'item_name'			=>	$_POST["hidden_name"],
-				'item_price'		=>	$_POST["hidden_price"],
+				'item_rental_price'		=>	$_POST["hidden_rental_price"],
 				'item_quantity'		=>	$_POST["quantity"]
 			);
 			$_SESSION["shopping_cart"][$count] = $item_array;
@@ -26,9 +26,9 @@ if(isset($_POST["add_to_cart"]))
 	else
 	{
 		$item_array = array(
-			'item_id'			=>	$_GET["id"],
+			'item_product_code'			=>	$_GET["product_code"],
 			'item_name'			=>	$_POST["hidden_name"],
-			'item_price'		=>	$_POST["hidden_price"],
+			'item_rental_price'		=>	$_POST["hidden_rental_price"],
 			'item_quantity'		=>	$_POST["quantity"]
 		);
 		$_SESSION["shopping_cart"][0] = $item_array;
@@ -41,7 +41,7 @@ if(isset($_GET["action"]))
 	{
 		foreach($_SESSION["shopping_cart"] as $keys => $values)
 		{
-			if($values["item_id"] == $_GET["id"])
+			if($values["item_product_code"] == $_GET["product_code"])
 			{
 				unset($_SESSION["shopping_cart"][$keys]);
 				echo '<script>alert("Item Removed")</script>';
@@ -63,7 +63,7 @@ if(isset($_GET["action"]))
 	      <a href="cosplay.php" class="btn btn-info" role="button">Cosplay</a></h3>
       <br/>
 			<?php
-				$query = "SELECT * FROM christmas ORDER BY id ASC";
+				$query = "SELECT * FROM product JOIN product_category using(category_id) WHERE category_id = 4 ORDER BY product_code ASC";
 				$result = mysqli_query($connect, $query);
 				if(mysqli_num_rows($result) > 0)
 				{
@@ -71,20 +71,20 @@ if(isset($_GET["action"]))
 					{
 				?>
 			<div class="col-md-4">
-				<form method="post" action="costumes.php?action=add&id=<?php echo $row["id"]; ?>">
+				<form method="post" action="costumes.php?action=add&product_code=<?php echo $row["product_code"]; ?>">
 					<div class="panel">
 					<div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
-						<img src="images/<?php echo $row["image"]; ?>" class="img-responsive" /><br />
+						<img src="images/<?php echo $row["product_image"]; ?>" class="img-responsive" /><br />
 
 						<h4 class="text-info"><?php echo $row["name"]; ?></h4>
 
-						<h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>
+						<h4 class="text-danger">$ <?php echo $row["rental_price"]; ?></h4>
 
 						<input type="text" name="quantity" value="1" class="form-control" />
 
 						<input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
 
-						<input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+						<input type="hidden" name="hidden_rental_price" value="<?php echo $row["rental_price"]; ?>" />
 
 						<input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
 
@@ -118,12 +118,12 @@ if(isset($_GET["action"]))
 					<tr>
 						<td><?php echo $values["item_name"]; ?></td>
 						<td><?php echo $values["item_quantity"]; ?></td>
-						<td>$ <?php echo $values["item_price"]; ?></td>
-						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
-						<td><a href="costumes.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+						<td>$ <?php echo $values["item_rental_price"]; ?></td>
+						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_rental_price"], 2);?></td>
+						<td><a href="costumes.php?action=delete&product_code=<?php echo $values["item_product_code"]; ?>"><span class="text-danger">Remove</span></a></td>
 					</tr>
 					<?php
-							$total = $total + ($values["item_quantity"] * $values["item_price"]);
+							$total = $total + ($values["item_quantity"] * $values["item_rental_price"]);
 						}
 					?>
 					<tr>
